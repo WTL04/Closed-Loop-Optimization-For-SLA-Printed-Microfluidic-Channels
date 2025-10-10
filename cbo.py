87b692f9f900dd9de909e03909b93be0af7e45d5
WTL04 <williamluu04@gmail.com>
2025-10-09 23:21:15 -0700

    scripts done, need to clean up later though

--- cbo.py
+++ cbo.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
 from bayes_opt import BayesianOptimization
 
-
-class CBO:
+class ContextualBayesOpt:
 
     """
     constructor:
@@ -22,11 +20,44 @@ class CBO:
 
     """
 
-    def __init__(self, df, pipeline, bounds, lam=2.0):
-        self.df = df.copy()
-        self.model = pipeline
-        self.bounds = bounds
-        self.lam = lam
+    def __init__(self, pipeline, pbounds, lam=2.0):
+        self.model = pipeline # sklearn pipeline
+        self.pbounds = pbounds # param search space
+        self.lam = lam # exploration hyperparam
+        self.pre = None
+        self.rf = None
+        self.EXPECTED = None
+        self.c_t = None
+
+    def train_surrogate(self, X, y, verbose=False):
+        param_grid = {
+            "rf__n_estimators": [100, 200, 300, 400, 500],
+            "rf__max_depth": [5, 10, 15, 20],
+            "rf__max_features": ["sqrt", "log2", None],
+        }
+        
+        # find best hyperparameters
+        grid_search = GridSearchCV(
+            estimator=self.model,
+            param_grid=param_grid,
+            cv=5, # num cross-validation folds
+            scoring='neg_mean_squared_error', # metric 
+            n_jobs=-1,
+            verbose=1 # display progress during fitting
+        )
+
+        grid_search.fit(X, y)
+
+        if verbose:
+            print("Best parameters found:", grid_search.best_params_)
+            print("Best cross-validation score:", grid_search.best_score_)
+
+        self.model = grid_search.best_estimator_
+        self._build_schema()
+        return self.model
+
+    def evalute_surrogate(self, X_test, y_test):
+        print("Surrogate model R² on test set:", self.model.score(X_test, y_test))
 
     # helper functions 
     def _build_schema(self):
@@ -34,5 +65,64 @@ class CBO:
         self.rf  = self.model.named_steps["rf"]
         self.EXPECTED = list(self.pre.feature_names_in_)
 
+    def _make_row(self, c_t, x):
+        # x contains knobs with strings, e.g. {"resin_type":"Resin_A", ...}
+        row = {**c_t, **x}  # merge current context + knobs
+        return pd.DataFrame([row], columns=self.EXPECTED) # EXPECTED from prerpocesser
 
+    def _mu_sigma(self, df):
+        """
+        Inputs dataframe of candidates with knobs + context 
+        Compute y_pred per tree in forest
+        Outputs arrays for mean and std, per row
+        """
+        Xt = self.pre.transform(df)
+        preds = np.vstack([t.predict(Xt) for t in self.rf.estimators_])  # [n_trees, n_samples]
+        return preds.mean(axis=0), preds.std(axis=0, ddof=1)
+
+
+    def _compute_ucb(self, x, lam=2.0):
+        """
+        x : dict
+            Full input row (knobs + current context).
+        lam : float
+            Exploration weight λ.
+        Returns
+        -------
+        float : UCB(x; λ)
+        """
+        import pandas as pd
+        X1 = pd.DataFrame([x], columns=self.EXPECTED)   # enforce schema
+        mu, sig = self._mu_sigma(X1)
+        return mu[0] + lam * sig[0]
+
+    def objective_ucb(self, **x):
+        row = self._make_row(self.c_t, x)            # merges knobs and current context
+        mu, sig = self._mu_sigma(row)
+        ucb = mu[0] + self.lam * sig[0]
+        return -ucb                   # BO maximizes (equiv to minimizing UCB)
+
+
+    def compute_bayes_opt(self, c_t, verbose=False):
+        self.c_t = c_t # set current context for objective
+        optimizer = BayesianOptimization(
+            f=self.objective_ucb,
+            pbounds=self.pbounds,
+            random_state=42,
+        )
+
+        optimizer.maximize(init_points=5, n_iter=20)
+
+        best_result = optimizer.max
+        best_params = best_result["params"]
+        best_ucb = -best_result["target"]
+
+        if verbose:
+            print("Best parameters found:")
+            print(best_params)
+
+            print("\nBest UCB score:")
+            print(best_ucb)
+        
+        return best_params, best_ucb, optimizer
 
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as numpy
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import seaborn as sns
+
+from cbo import ContextualBayesOpt
 
 
 def visualize_control_chart(df_batches):
@@ -80,28 +83,42 @@ def main():
         ("num", StandardScaler(), numerical)
     ])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    X = df_batches[features] # inputs
-    y = df_batches["cv"] # targets
-
-    categorical = ["resin_type", "support_mode"]
-    numerical = [f for f in features if f not in categorical]
-
-    preprocess = ColumnTransformer([
-        ("cat", OneHotEncoder(handle_unknown="ignore", sparse_output=False), categorical),
-        ("num", StandardScaler(), numerical)
+    pipeline = Pipeline([
+        ("preprocess", preprocess),
+        ("rf", RandomForestRegressor(random_state=42))
     ])
 
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    user_input = input("Visualze Drift Relationship (1) or Control Chart (2)?: ")
+    # user_input = input("Visualze Drift Relationship (1) or Control Chart (2)?: ")
+    #
+    # if user_input == 1:
+    #     visualize_CV_Drift_relationship(df_batches)
+    # elif user_input == 2:
+    #     visualize_control_chart(df_batches)
+    #
 
-    if user_input == 1:
-        visualize_CV_Drift_relationship(df_batches)
-    elif user_input == 2:
-        visualize_control_chart(df_batches)
+    # BO search space (knobs)
+    pbounds = {
+        "layer_thickness_um": (50, 100), # limited to 50 and 100 per dr. ava's comment
+        "orientation_deg": (0, 90),
+        "fit_adjustment_pct": (-2.0, 2.0),
+        "resin_type": ("Resin_A", "Resin_B", "Resin_C"), # A = 1, B = 2, C = 3, when encoded
+        "support_mode": ("auto", "manual") # auto = 0, manual = 1, when encoded
+    }
 
-    c_t = input("Enter Current Context: ")
+    # current context snapshot BEFORE a print (experimental)
+    c_t = {
+        "ambient_temp": 72,    # °F, lab measurement
+        "resin_temp": 76,      # °F, sensor reading
+        "resin_age": 12.0,        # days since resin was opened
+    } 
+    
+    #---- BayesOpt Initial Exporlation---
+    model = ContextualBayesOpt(pipeline=pipeline, pbounds=pbounds)
+    model.train_surrogate(X_train, y_train, verbose=True)
+    model.compute_bayes_opt(c_t, verbose=True)
 
     
 
