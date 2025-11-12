db8175bf2c5dc10d27a965eb9897bec14753996e
WTL04 <williamluu04@gmail.com>
2025-11-11 19:57:58 -0800

    renamed main.py -> cbo_single_run.py, renamed phase2_closed_loop.py -> cbo_closed_loop.py, turned visualizations into function and added it to visualize.py

--- visualize.py
+++ visualize.py
@@ -10,7 +10,7 @@ def visualize_control_chart(df_batches):
     plt.legend()
     plt.show()
 
-def visualize_CV_Drift_relationship(df_batches):
+def visualize_cv_drift_relationship(df_batches):
     for c in ["ambient_temp", "resin_temp", "resin_age"]:
         sns.regplot(
             x=df_batches[c],
@@ -23,4 +23,24 @@ def visualize_CV_Drift_relationship(df_batches):
         plt.ylabel("CV")
         plt.show()
 
+def visualize_cv_histories(all_histories):
+    plt.figure(figsize=(8, 4.5))
+    for idx, hist in enumerate(all_histories):
+        plt.plot(
+            range(len(hist)), hist, marker="o", linestyle="-", label=f"Run {idx + 1}"
+        )
+
+        # highlight best point
+        best_i = np.argmin(hist)
+        plt.scatter(best_i, min(hist), color="red", marker="*", s=120)
+
+    plt.title(
+        "Flow Rate CV Improvement Across Iterations (Fixed Context: Warm Room Temp, 5-Day Resin)"
+    )
+    plt.xlabel("Iteration")
+    plt.ylabel("Coefficient of Variation (CV)")
+    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.legend(title="Independent Runs")
+    plt.tight_layout()
+    plt.show()
 
